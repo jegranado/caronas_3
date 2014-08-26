4f431ff9ecdc70f0f6e5bc7338f3af750339f6ec
jegranado <jegranado@gmail.com>
2014-08-26 00:04:24 -0300

    xvv

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -34,12 +34,24 @@
                 getCaronas();
             intel.xdk.notification.hideBusyIndicator();
         });
+        $(document).on("click", ".uib_w_4", function(evt)
+        {
+         activate_subpage("#cadastrosub"); 
+        });
+        $(document).on("click", ".uib_w_12", function(evt)
+        {
+         activate_subpage("#mainsub"); 
+        });
+        $(document).on("click", "#btn_sair", function(evt)
+        {
+         activate_subpage("#mainsub"); 
+        });
 }
  $(document).ready(register_event_handlers);
 })();
 
 	function getCaronas(){
-		$.ajax({url:"http://ec2-54-191-186-213.us-west-2.compute.amazonaws.com:8033/ws/getcaronas.asp",async: false}).done(function( data ) {
-            document.getElementById("bs-accordion-0").innerHTML=data;
+		$.ajax({url:"http://ec2-54-191-186-213.us-west-2.compute.amazonaws.com:8033/ws/getcaronas.asp"}).done(function( data ) {
+            document.getElementById("container_caronas").innerHTML=data;
         });	                       
     }
